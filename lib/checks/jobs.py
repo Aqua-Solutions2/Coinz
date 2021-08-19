668e67190fb053adde81fceb318965a0a56f6949
Siebe Baree <siebe.baree@outlook.com>
2021-08-19 17:13:11 +0200

    Updated File.

--- lib/checks/jobs.py
+++ lib/checks/jobs.py
@@ -1,21 +1,31 @@
 from lib.db import db
 
-DEFAULT = ((0, 0, "Cashier", 0, 210),
-           (0, 0, "Janitor", 0, 250),
-           (0, 0, "Housekeeper", 0, 280),
-           (0, 0, "Teacher", 0, 340),
-           (0, 0, "Construction Worker", 0, 390),
-           (0, 0, "Mechanic", 0, 460),
-           (0, 0, "Electrician", 0, 490),
-           (0, 0, "Police Officer", 0, 550),
-           (0, 0, "Lawyer", 0, 680),
-           (0, 0, "Software Developer", 0, 720),
-           (0, 0, "Dentist", 0, 800))
+DEFAULT = ((0, 0, "cashier", 0, 210),
+           (0, 0, "janitor", 1, 250),
+           (0, 0, "housekeeper", 2, 280),
+           (0, 0, "teacher", 3, 340),
+           (0, 0, "construction worker", 5, 390),
+           (0, 0, "mechanic", 7, 460),
+           (0, 0, "electrician", 8, 490),
+           (0, 0, "police officer", 10, 550),
+           (0, 0, "lawyer", 12, 680),
+           (0, 0, "software Developer", 16, 720),
+           (0, 0, "dentist", 20, 800))
 
 
 def get_jobs(guild_id):
-    all_jobs = db.records("SELECT * FROM guildWorkPayouts WHERE GuildID = %s", guild_id)
-    return DEFAULT if all_jobs == () else all_jobs
+    all_jobs = db.records("SELECT * FROM guildWorkPayouts WHERE GuildID = %s ORDER BY Loon", guild_id)
+    return DEFAULT if not all_jobs else all_jobs
+
+
+def get_job(current_job, all_jobs):
+    for job in all_jobs:
+        if current_job == job[2]:
+            job_tuple = job
+            break
+    else:
+        job_tuple = (0, 0, "unemployed", 0, 0)
+    return job_tuple
 
 
 def set_job(guild_id, job_name, lvl, loon):
